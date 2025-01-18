7e48520030b6db99c36946005674a3a68ad27cd0
IdealistCat <mattallan.h@gmail.com>
2025-01-18 15:35:52 -0600

    we got 1.3 les goo

--- gb_changeloggen.py
+++ gb_changeloggen.py
@@ -3,10 +3,17 @@
 # 1.0 - Base Script
 # 1.1 - Directory var, tab support, a tab character variable, and a github page!
 # 1.2 - missing "/" at the end of directory check, and quotes for the directory of the "written to" text
+# 1.3 - if there is no entry type then it gets ignored,
+# added some rules for gamebanana changelog entrys, 
+# there are input fields for the things you want displayed in the progress print
 
 # Written by Sphis_Sinco for "Fun over Money".
 # This cool little minecraft (java edition) mod ;)
 # https://gamebanana.com/mods/569653
+
+# display variables
+display_progress = input('Display Progress % (y/n)?\n').lower() == 'y'
+display_tabsremoved = input('Display Character Tabs Removed (y/n)?\n').lower() == 'y'
 
 # this controls the directory
 directory = "developer/scripts/gamebanana_changelog_generator"
@@ -41,7 +48,9 @@ def checkFor(checking, prefix, suffix):
 
     if checking.startswith(f"- {prefix} ") :
         returnval = checking.removeprefix(f"- {prefix} ")
-        returnval = returnval + f", {suffix}"
+
+        if returnval.__len__() >= 10:
+            returnval = returnval + f", {suffix}"
 
         return returnval
 
@@ -49,6 +58,8 @@ def checkFor(checking, prefix, suffix):
 
 index = 0
 for entry in array_logs:
+    valid = False
+
     entry1 = entry
     check = entry1
     tabcount = 0
@@ -65,19 +76,33 @@ for entry in array_logs:
         addition = checkFor(addition, "Reworked", "Overhaul")
         addition = checkFor(addition, "Changed", "Tweak")
         addition = checkFor(addition, "Fixed", "Bugfix")
+        
+        if not addition.__len__() < 10 or addition.startswith('- '):
+            valid = True
 
-    gamebanana_changelog = gamebanana_changelog + addition
-
-    # prevent extra line break
-    if index + 1 != array_logs.__len__():
-        gamebanana_changelog = gamebanana_changelog + "\n"
+    if valid:
+        gamebanana_changelog = gamebanana_changelog + addition
+    
+        # prevent extra line break
+        if index + 1 != array_logs.__len__():
+            gamebanana_changelog = gamebanana_changelog + "\n"
 
 
     index=index + 1
     progress = progress + 1
 
     # print progress
-    print(f"Progress: {round(progress/max_progress*100)}% | removed {tabcount} \"{tab_char_len} character\" tabs")
+    progressTXT = f"Progress: {round(progress/max_progress*100)}%"
+    removed_chartab = f"removed {tabcount} \"{tab_char_len} character\" tabs"
+
+    full_text = ""
+
+    if display_progress: full_text = full_text + progressTXT + ' | '
+    if display_tabsremoved: full_text = full_text + removed_chartab + ' | '
+
+    full_text = full_text.removesuffix(' | ')
+
+    print(full_text)
 
 
 # write to the final_log.txt file
